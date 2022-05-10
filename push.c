c74fa2d81378775c59cd7d585a4dfd17da2c3746
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-10 12:17:46 +0000

    add

--- push.c
+++ push.c
@@ -7,7 +7,6 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *ptr;
 	stack_t *newNode = malloc(sizeof(stack_t));
 
 	if (isdigit(data) != 0)
@@ -15,23 +14,24 @@ void push(stack_t **stack, unsigned int line_number)
 		printf("L%d: usage: push integer", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", data);
 	if (!stack || !*stack)
 	{
 		newNode->n = data;
 		newNode->next = *stack;
 		newNode->prev = NULL;
 		*stack = newNode;
+		printf("%d\n", (*stack)->n);
 	}
 	else
 	{
 		while ((*stack)->next != NULL)
 		{
-			ptr = ptr->next;
+			*stack = (*stack)->next;
 		}
 		newNode->n = data;
 		newNode->next = NULL;
 		newNode->prev = *stack;
 		(*stack)->next = newNode;
+		printf("%d\n", (*stack)->next->n);
 	}
 }
