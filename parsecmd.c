e3adae521abfe9eacfb1ca8fe80aef810e631185
Aleph235 <akram239911@gmail.com>
2022-05-10 17:03:03 +0200

    parse it properly

--- parsecmd.c
+++ parsecmd.c
@@ -9,16 +9,18 @@ void parsecmd(stack_t **stack, unsigned int lineNumber, char *lineToParse)
 								{NULL, NULL}};
 
 	splitLine = split_str(lineToParse, "\t\n ");
-	if (splitLine[1])
-	{
-		data = atoi(splitLine[1]);
-	}
+	
 	while (instruct[i].opcode != NULL)
 	{
 		if (!splitLine[0])
 			break;
 		if (strcmp(splitLine[0], instruct[i].opcode) == 0)
 		{
+			if (strcmp(splitLine[0] , "push"))
+			{
+				if (!splitLine[1] || isdigit(splitLine[1]) != 0)
+					fprintf(stderr, "L%d: usage: push integer", lineNumber);
+			}
 			instruct[i].f(stack, lineNumber);
 		}
 		i++;
