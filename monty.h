b03447e3dfebf0d626a4b3a5bf7eefd59273636e
Aleph235 <akram239911@gmail.com>
2022-05-11 11:34:03 +0200

    division function in stdlib is causing conflict with div.c

--- monty.h
+++ monty.h
@@ -2,14 +2,14 @@
 #define HEADER_FILE
 #define _GNU_SOURCE
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdbool.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -48,6 +48,8 @@ void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 char **split_str(char *input, char *delimiter);
 bool isNumber(char *s);
 
