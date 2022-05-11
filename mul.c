333d0c8da43669e63c2d9be186ee78fc24fb267c
Aleph235 <akram239911@gmail.com>
2022-05-11 14:02:09 +0200

    multiplication function

---
+++ mul.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * mul - multiplies the last two elements of the stack
+ *@stack: stack of file lines
+ *@line_number: number of file lines
+ *
+ * Return: void
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (*stack && (*stack)->prev)
+	{
+		(*stack)->prev->n *= (*stack)->n;
+		pop(stack, line_number);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
