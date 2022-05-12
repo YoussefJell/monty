aecb59602e94fa2a0729d7c8166d13d7650d6741
Aleph235 <akram239911@gmail.com>
2022-05-12 19:19:24 +0200

    refacting ispush fuction

--- isPush.c
+++ isPush.c
@@ -10,22 +10,12 @@ void isPush(char *pushCMD, char *pushNUM, unsigned int lineNumber)
 {
 	if (strcmp(pushCMD, "push") == 0)
 	{
-		if (pushNUM)
-		{
-			if (isNumber(pushNUM))
-				data = atoi(pushNUM);
-			else
-			{
-				fprintf(stderr, "L%d: usage: push integer\n", lineNumber);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
+		if (!pushNUM || !isNumber(pushNUM))
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", lineNumber);
 			exit(EXIT_FAILURE);
 		}
+		else
+			data = atoi(pushNUM);
 	}
-	else
-		return;
 }
